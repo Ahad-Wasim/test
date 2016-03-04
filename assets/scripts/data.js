337a1479411c37f05b069b64ab9d494d05240a29
Ahad Wasim <ahadwasim@gmail.com>
2016-03-04 12:54:41 -0800

    my mvp version

---
+++ assets/scripts/data.js
@@ -0,0 +1,28 @@
+// At some point needs to passed down to dropdown function
+(function(){
+  window.languages = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+  ]; 
+}())
+
